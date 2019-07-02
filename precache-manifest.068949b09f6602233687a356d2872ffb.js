daed71058648ec48522df840c0ce82c5ed2116fa
zhuhailiang4 <zhuhailiang@jd.com>
2019-07-02 14:02:56 +0800

    deploy

---
+++ precache-manifest.068949b09f6602233687a356d2872ffb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7541ec1b8a1fac27e54",
+    "url": "/js/about.556e80a3.js"
+  },
+  {
+    "revision": "54b4d450dba48498b115",
+    "url": "/css/app.b7b29f14.css"
+  },
+  {
+    "revision": "54b4d450dba48498b115",
+    "url": "/js/app.c04175cd.js"
+  },
+  {
+    "revision": "b0c9a04cbec1509a09bb",
+    "url": "/js/chunk-vendors.70a97d3c.js"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "0ba0cd0ce5e39426bcf29eb1505b5102",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
